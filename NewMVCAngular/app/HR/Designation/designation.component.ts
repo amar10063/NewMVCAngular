1aabb4fcce4ce73890812f21f8788d81062886f0
amar10063 <amar10063@yahoo.co.in>
2019-01-02 14:31:14 +0530

    added designation component files

--- NewMVCAngular/app/HR/Designation/designation.component.ts
+++ NewMVCAngular/app/HR/Designation/designation.component.ts
@@ -1,1 +1,7 @@
-﻿
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'designation',
+    templateUrl: './designation.component.html',
+})
+export class DesignationComponent { }
